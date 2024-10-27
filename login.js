35b145c650ba120edcb6c611738b14d95a2c8375
Himanshi Jolly <148650501+HimanshiJolly@users.noreply.github.com>
2024-10-28 00:06:41 +0530

    Update login.js

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ document.getElementById('loginform').addEventListener('submit', function(event) 
     console.log('Stored Login Username:', localStorage.getItem('login-username'));
     console.log('Stored Login Password:', localStorage.getItem('login-password'));
     alert('Logged in successfully!');
-
+    window.location.href = 'main.html';
     // Clear inputs
     document.getElementById('login-username').value = '';
     document.getElementById('login-password').value = '';
@@ -41,9 +41,56 @@ document.getElementById('registerform').addEventListener('submit', function(even
     console.log('Stored Registration Email:', localStorage.getItem('register-email'));
     console.log('Stored Registration Password:', localStorage.getItem('register-password'));
     alert('user registered successfully!');
-
+     window.location.href = 'main.html'; 
     // Clear inputs
     document.getElementById('register-username').value = '';
     document.getElementById('register-email').value = '';
     document.getElementById('register-password').value = '';
+});
+
+
+//navbar
+
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+
+// js code to toggle dark and light mode
+      modeToggle.addEventListener("click" , () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+
+        // js code to keep user selected mode even page refresh or file reopen
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode" , "light-mode");
+        }else{
+            localStorage.setItem("mode" , "dark-mode");
+        }
+      });
+
+// js code to toggle search box
+        searchToggle.addEventListener("click" , () =>{
+        searchToggle.classList.toggle("active");
+      });
+ 
+      
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
 });
